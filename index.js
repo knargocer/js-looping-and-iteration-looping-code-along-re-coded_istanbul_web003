44e242693111d912e932b29a473fae80428a8556
Knar Gocer <knargr@gmail.com>
2020-07-21 09:22:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// Code your solutions in this file
-const gifts = ["teddy bear", "drone", "doll"];
- 
-function wrapGift(gift) {
-  console.log(`Wrapped ${gift} and added a bow!`);
+function writeCards(x,y){
+  for(let i=0; i<)
 }
-wrapGift(gifts[0]);
-wrapGift(gifts[1]);
-wrapGift(gifts[2]);
-
-for (let age = 30; age < 40; age++) {
-  console.log(`I'm ${age} years old. Happy birthday to me!`);
-}
-
-
- 
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-  }
- 
-  return gifts;
-}
- 
-wrapGifts(gifts);
