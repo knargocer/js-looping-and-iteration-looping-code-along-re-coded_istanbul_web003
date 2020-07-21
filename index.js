0a8fd594661f9bc483164a8437be117f6b58481a
Knar Gocer <knargr@gmail.com>
2020-07-21 09:18:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+ 
+function wrapGift(gift) {
+  # For Ruby or Pythonistas, note that the " is now a ` (back-tick)
+  # We'll discuss interpolation in detail elsewhere, but note that
+  # JavaScript uses ` like Ruby uses ".
+  console.log(`Wrapped ${gift} and added a bow!`);
+}
