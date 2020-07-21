738feaef6afa58b0168bcf89fbbf46b79ee5bd55
Knar Gocer <knargr@gmail.com>
2020-07-21 09:29:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function writeCards(name, evtname){
   let counter=0;
   const message=[];
-  for(let i=0; i<x.length;i++){
+  for(let i=0; i<name.length;i++){
     counter++;
-    message.push("Thank you, " + x[i]+", for the wonderful surprise gift!");
+    message.push("Thank you, " + name[i]+", for the wonderful surprise gift!");
   }
   return message;
 }
