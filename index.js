13512f22aafd028bd599748872a199d09b3798c8
Knar Gocer <knargr@gmail.com>
2020-07-21 09:20:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,15 @@ wrapGift(gifts[2]);
 for (let age = 30; age < 40; age++) {
   console.log(`I'm ${age} years old. Happy birthday to me!`);
 }
+
+
  
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+ 
+  return gifts;
+}
+ 
+wrapGifts(gifts);
